d3d33dd67afa945bef5e808901b094d15f6416e3
KoriCsabi <koricsanszky.csaba@gmail.com>
2016-01-24 19:08:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 header('Content-Type: text/plain');  // Setting the header type to formatting the tabulators
 
-$dbHost = "localhost";  // Set mysql host
-$dbUser = "root";		// Set mysql username
-$dbPass = "";			// Set mysql password
+$dbHost = "localhost";	// Set mysql host
+$dbUser = "root";	// Set mysql username
+$dbPass = "";	// Set mysql password
 $dbName = "goldman";	// Set mysql database name
 
 mysql_connect($dbHost, $dbUser, $dbPass) or die("Sikertelen kapcsolódás az adatbázishoz!");  // Try to connect to the mysql server, if get back a response when die
@@ -27,11 +27,11 @@ $stat_datas = mysql_query($stat);  // Run the statistic query
 
 echo "Tárgy nap\tBanner\t\t\tÖsszeg\r\n";  // Write the headline of table
 while($dataline = mysql_fetch_object($stat_datas)){ // Processing the statistics query in object
-	echo str_replace("-", ".", $dataline -> day);  // Get the day
-	echo "\t"; 					// Formatting to table with tabulators
-	echo $dataline -> name; 	// Get the name of banner
-	echo "\t";					// Formatting to table with tabulators
+	echo str_replace("-", ".", $dataline -> day);	// Get the day
+	echo "\t";	// Formatting to table with tabulators
+	echo $dataline -> name;	// Get the name of banner
+	echo "\t";	// Formatting to table with tabulators
 	echo $dataline -> amount;	// Get the amount of each banners
-	echo " $\r\n";				// Break the line 
+	echo " $\r\n";	// Break the line 
 }  // End of statistic query process
 ?>
